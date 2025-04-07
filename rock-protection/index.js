a15ce77a24fcbfae7c79a1cf726cbcab38681bb3
grant freeman <grantfreeman@pm.me>
2025-04-07 05:43:49 -0700

    standardjs cleanup

--- rock-protection/index.js
+++ rock-protection/index.js
@@ -1,16 +1,15 @@
 /*************************************************************************************************
- * index.js v1.1.0                                                                               *
+ * index.js v1.1.1                                                                               *
  * (c) 2025 Grant Freeman                                                                        *
  * License GPL 3.0                                                                               *
  *************************************************************************************************/
-/* global ProChart */
+/* global Pro, ProChart */
 
 const pc = new ProChart('pro-chart-container', true)
 pc.filter.range.min = 20
 pc.filter.range.max = 70
 pc.filter.strength.min = 5
 pc.deploy()
-
 
 const appendixEvO = new ProChart('expansion-vs-operation', false)
 appendixEvO.prune((pro) => {
